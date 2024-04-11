99ee60b17ffcfd6cfd180bb4a662ae2e586a966e
Mukesh Kushwaha <99179878+RAKESHKUSHWAHA7518@users.noreply.github.com>
2024-04-11 19:31:14 +0530

    connect mongoose atlas

---
+++ src/index.js
@@ -0,0 +1,43 @@
+ 
+//  require('dotenv').config({path: './env'})
+
+ import dotenv from 'dotenv'
+  
+import connectDB from "./db/index.js";
+
+ dotenv.config({
+    path: './env'
+ })
+
+ 
+
+  connectDB()
+
+
+
+
+
+
+
+
+
+ /*
+ import express from "express";
+ const app = express();
+
+ (async()=>{
+    try{
+   mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+   app.on ("error",(error)=>{
+    console.log("error:" ,error);
+    throw error;
+   })
+   app.listen(process.env.PORT,()=>{
+    console.log("listening on port ${process.env.PORT}");
+   })
+    } catch(error){
+        console.error( "error",error);
+    }
+ })()
+
+ */
